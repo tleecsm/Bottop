6961e5e9c51880ad10d1ebdf8e6c8488bf5ec30f
Tanner Lee <tannerlee@mymail.mines.edu>
2018-06-12 17:51:15 -0400

    Updated linux installer for Opus

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" 
 sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
 sudo apt-get update && sudo apt-get install libsodium-dev;
 sudo apt-get install build-essential
+sudo apt-get install opus-tools
+sudo apt-get install libav-tools
 sudo python3.5 get-pip.py
 python3 -m pip install -U discord.py[voice]
 pip install imgurpython
